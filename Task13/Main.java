4eacd40e84b7a4d34e33fea6ee5a07d8c890a98c
cjomarrod <cjoms0616@gmail.com>
2025-05-30 13:39:44 +0800

    Add files via upload

---
+++ Task13/Main.java
@@ -0,0 +1,11 @@
+package Task13;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        Car myCar = new Car("Toyota", "Camry", 2022, 4);
+        
+       
+        myCar.displayDetails();
+    }
+}
